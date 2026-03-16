9484fe962b073092cacd0742ff7ed127df71e638
leejunhee8235 <leejunhee8235@gmail.com>
2026-03-16 23:56:40 +0900

    3/16 문제풀이

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,70 @@
-# 스택 - 스택 (백준 실버 4)
-# 문제 링크: https://www.acmicpc.net/problem/10828
+# # 스택 - 스택 (백준 실버 4)
+# # 문제 링크: https://www.acmicpc.net/problem/10828
+# 정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+# 명령은 총 다섯 가지이다.
+
+# push X: 정수 X를 스택에 넣는 연산이다.
+# pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 
+# 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 스택에 들어있는 정수의 개수를 출력한다.
+# empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+# top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# 입력
+# 첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 
+# 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 주어지는 정수는 1보다 크거나 같고, 
+# 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
+
+# 출력
+# 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+
+
+
+# 1. 명령 개수 입력받기
+M = int(input())
+
+# 2. 스택으로 사용할 빈 리스트 만들기
+stack = []
+# 3. 출력값들을 저장할 리스트 만들기
+save = []
+# 4. 명령 개수만큼 반복
+for _ in range(M):
+    
+    # 4-1. 한 줄 입력받기
+    O = input()
+    O = O.split()
+    # 4-2. 만약 명령이 "push X" 형태라면
+    if O[0] == "push":
+        stack.append(int(O[1]))
+
+    # 4-3. 만약 명령이 "pop" 이라면
+    if O[0] == "pop":
+       if not stack:
+           save.append(-1)
+       else:
+           save.append(stack.pop())
+
+    # 4-4. 만약 명령이 "size" 라면
+    if O[0] == "size":
+        save.append(len(stack))
+
+    # 4-5. 만약 명령이 "empty" 라면
+    if O[0] == "empty":
+        if not stack:
+           save.append(1)
+        else:
+           save.append(0)
+
+
+    # 4-6. 만약 명령이 "top" 이라면
+    if O[0] == "top":
+        if not stack:
+            save.append(-1)
+        else:
+            save.append(stack[-1])
+
+
+
+# 5. 저장한 출력값들을 한 줄씩 출력
+for i in save:
+    print(i)
--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,44 @@
-# 이분탐색 - 수 찾기 (백준 실버4)
-# 문제 링크: https://www.acmicpc.net/problem/1920
+# # 이분탐색 - 수 찾기 (백준 실버4)
+# # 문제 링크: https://www.acmicpc.net/problem/1920
+# 문제
+# N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 
+# 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 
+# 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다. 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 
+# 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다. 
+# 모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다.
+
+# 출력
+# M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+N = int(input())  # 첫째 줄: 숫자 개수 N 입력
+
+A = list(map(int, input().split()))  # 둘째 줄: N개의 정수를 공백 기준으로 나눠 리스트로 저장
+
+M = int(input())  # 셋째 줄: 찾을 숫자 개수 M 입력
+
+targets = list(map(int, input().split()))  # 넷째 줄: 실제로 찾아볼 숫자들을 리스트로 저장
+
+A.sort()  # 이분 탐색을 하려면 정렬이 필요하므로 A를 오름차순 정렬
+
+for target in targets:  # 찾아볼 숫자들을 하나씩 꺼내서 검사
+    left = 0  # 탐색 시작 위치(맨 왼쪽 인덱스)
+    right = len(A) - 1  # 탐색 끝 위치(맨 오른쪽 인덱스)
+    found = 0  # 아직 target을 못 찾았다고 가정하고 0으로 시작
+
+    while left <= right:  # 탐색 범위가 남아 있는 동안 반복
+        mid = (left + right) // 2  # 현재 탐색 범위의 중간 인덱스 계산
+
+        if A[mid] == target:  # 중간값이 찾는 값과 같으면
+            found = 1  # 찾았으므로 1로 표시
+            break  # 더 볼 필요 없으니 while문 종료
+
+        elif A[mid] < target:  # 중간값이 찾는 값보다 작으면
+            left = mid + 1  # target은 오른쪽에 있으므로 left를 mid 다음으로 이동
+
+        else:  # 위 두 경우가 아니면 A[mid] > target 이라는 뜻
+            right = mid - 1  # target은 왼쪽에 있으므로 right를 mid 이전으로 이동
+
+    print(found)  # 찾았으면 1, 못 찾았으면 0 출력
